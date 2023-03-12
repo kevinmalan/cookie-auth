820576c23df73ca593fd5f4dbb6a2db10bf1655e
kevinmalan <kmalan@outlook.com>
2023-03-12 11:21:04 +0200

    Small fixes

---
+++ API/API/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class HealthController : BaseController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("cookieauth-api.localhost.com is running...");
+        }
+    }
+}
--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,10 +1,18 @@
 using Core.Contexts;
 using Core.Interfaces;
 using Core.Services;
+using Microsoft.AspNetCore.Authentication.Certificate;
 using Microsoft.EntityFrameworkCore;
 using Shared.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
+    .AddCertificate(o =>
+    {
+        o.AllowedCertificateTypes = CertificateTypes.All;
+    });
 
 builder.Configuration.AddJsonFile("appsettings.json", false, true);
 builder.Services.AddDbContext<DataContext>(o => o.UseSqlServer(builder.Configuration["Sql:ConnectionString"]), ServiceLifetime.Transient);
@@ -22,6 +30,9 @@ builder.Services.AddTransient<ITokenService, TokenService>();
 builder.Services.Configure<TokenConfig>(builder.Configuration.GetSection("Token"));
 builder.Services.Configure<PasswordConfig>(builder.Configuration.GetSection("Password"));
 
+// TODO: IIS with cookieauth-api.localhost.com
+// Then install the cert and ref it here somewhere
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -31,6 +42,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
